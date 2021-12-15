3be6e73c325e2f84a41e8505763502b9f062dc24
Artogerilyagames <91071185+Artogerilyagames@users.noreply.github.com>
2021-12-15 13:29:29 +0800

    50. Crosshair In Air Factor

--- Source/Shooter/ShooterCharacter.cpp
+++ Source/Shooter/ShooterCharacter.cpp
@@ -282,7 +282,27 @@ void AShooterCharacter::CalculateCrosshairSpread(float DeltaTime)
 	(WalkSpeedRange,
 		VelocityMultiplierRange,
 		Velocity.Size());
-	CrosshairSpreadMultiplier = 0.5f + CrosshairVelocityFactor;
+	
+	if(GetCharacterMovement()->IsFalling())
+	{
+		CrosshairInAirFactor = FMath::FInterpTo
+		(CrosshairInAirFactor,
+			2.25f,
+			DeltaTime,
+			2.25f);
+	}
+	else
+	{
+		CrosshairInAirFactor = FMath::FInterpTo
+	(CrosshairInAirFactor,
+		0.f,
+		DeltaTime,
+		30.f);
+	}
+	CrosshairSpreadMultiplier =
+		0.5f +
+			CrosshairVelocityFactor +
+				CrosshairInAirFactor;
 }
 
 // Called every frame
