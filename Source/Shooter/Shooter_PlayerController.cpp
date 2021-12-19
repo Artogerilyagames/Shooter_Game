d4ff3a4b44eb397117bc3b89b847c556d6faaa9c
Chris Paz <balckpadesgames@gamil.com>
2021-12-19 16:10:38 +0800

    91. Ammo Count 92. Draw Ammo Count to Screen

---
+++ Source/Shooter/Shooter_PlayerController.cpp
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Shooter_PlayerController.h"
+#include "Blueprint/UserWidget.h"
+
+AShooter_PlayerController::AShooter_PlayerController()
+{
+	
+}
+
+void AShooter_PlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	if(HUDOverlayClass)
+	{
+		HUDOverlay = CreateWidget<UUserWidget>(this, HUDOverlayClass);
+		if(HUDOverlay)
+		{
+			HUDOverlay->AddToViewport();
+			HUDOverlay->SetVisibility(ESlateVisibility::Visible);
+		}
+	}
+}
---
+++ Source/Shooter/Shooter_PlayerController.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Shooter_PlayerController.generated.h"
+
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTER_API AShooter_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+public:
+	AShooter_PlayerController();
+protected:
+	virtual void BeginPlay() override;
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= Widgets, meta = (AllowPrivateAccess = "true"));
+	TSubclassOf<class UUserWidget> HUDOverlayClass;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category =Widgets, meta = (AllowPrivateAccess = "true"));
+	UUserWidget* HUDOverlay;
+	
+};
