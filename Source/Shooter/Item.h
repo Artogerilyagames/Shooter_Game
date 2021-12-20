dc4b82de673390e9516a7aa9ee78e3c005d52002
Chris Paz <balckpadesgames@gamil.com>
2021-12-20 21:03:59 +0800

    110. PickupSound Update

--- Source/Shooter/Item.h
+++ Source/Shooter/Item.h
@@ -87,6 +87,10 @@ private:
 	TArray<bool> ActiveStars;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category= "Item Properties", meta = (AllowPrivateAccess = "true"))
 	EItemState ItemState;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= "Item Properties", meta = (AllowPrivateAccess = "true"))
+	class USoundCue* PickupSound;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= "Item Properties", meta = (AllowPrivateAccess = "true"))
+	USoundCue* EquipSound;
 	
 public:
 	FORCEINLINE UWidgetComponent* GetPickupWidget() const {return PickupWidget;}
@@ -95,5 +99,8 @@ public:
 	FORCEINLINE EItemState GetItemState() const { return ItemState;}
 	FORCEINLINE void SetItemState(EItemState State);
 	FORCEINLINE USkeletalMeshComponent* GetItemMesh() const {return ItemMesh;}
+	FORCEINLINE USoundCue* GetPickupSound() const {return PickupSound;}
+	FORCEINLINE USoundCue* GetEquipSound() const {return EquipSound;}
+	
 
 };
